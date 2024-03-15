f9c02ae62ff0808a6d1445262ceb975661f8086c
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-15 15:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 //your JS code here. If required.
-document.body.onload = addElement;
-
-  function addElement() {
-    // create a new div element
-    const newDiv = document.createElement("p");
+document.addEventListener("DOMContentLoaded",() => {
+  document.body.innerHTML = "<p>DOM load success</p>";
   
-    // and give it some content
-    const newContent = document.createTextNode("DOM load success");
-  
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-  
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
-  }
+})
